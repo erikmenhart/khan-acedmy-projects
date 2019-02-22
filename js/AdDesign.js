ff7db294cc894138604a76150203df5427751423
erikmenhart <36042405+erikmenhart@users.noreply.github.com>
2019-02-22 13:37:30 -0500

    Create AdDesign.js

---
+++ js/AdDesign.js
@@ -0,0 +1,12 @@
+fill(255, 0, 0);
+textSize(30);
+text("Super Pizza", 10, 30);
+textSize(10);
+text("SO SOFT! DON'T GO TO SLEEP WITHOUT ONE!", 10, 87);
+
+triangle(44,115,151,200,114,90);
+triangle(205,97,113,91,150,200);
+triangle(206,97,254,150,150,200);
+
+textSize(40);
+text("eat pizza 50% off" ,100,250);
